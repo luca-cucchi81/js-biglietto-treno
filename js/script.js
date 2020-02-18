53e2fde083a211e3d3a5e266e3c39f5716b4c5fc
Luca <lu.cucchi@gmail.com>
2020-02-18 16:26:27 +0100

    fix vari

--- js/script.js
+++ js/script.js
@@ -20,9 +20,9 @@ document.getElementById('età').innerHTML = età;
 document.getElementById('kilometri').innerHTML = tragitto;
 
 /*inserimento variabili prezzo*/
-var prezzoIntero = tragitto * 0.21;
-var prezzoAdolescenti = (prezzoIntero * 80) / 100;
-var prezzoAnziani = (prezzoIntero * 60) / 100;
+var prezzoIntero = Math.ceil(tragitto * 0.21);    //i prezzi vengono arrotondati sempre per eccesso... "grazie Trenitalia!!"
+var prezzoAdolescenti = Math.ceil((prezzoIntero * 80) / 100);
+var prezzoAnziani = Math.ceil((prezzoIntero * 60) / 100);
 
 /*inserimento istruzioni condizionali*/
 if (età < 18){
